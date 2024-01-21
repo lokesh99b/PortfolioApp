6df2f894c12e96c220ccb09e2fe1e5b4cd49efd7
Lokesh <lokeshb031@gmail.com>
2024-01-21 10:40:28 +0530

    Updates

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.JPG")
+    st.image("images/photo.JPG", width=300)
 
 with col2:
     st.title("Lokesh Bhosale")
