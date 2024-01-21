c552d7ac619bf1beb883597498fbd5e71ccbfca8
Lokesh <lokeshb031@gmail.com>
2024-01-21 10:54:39 +0530

    Updates

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -5,14 +5,16 @@ from send_email import send_email
 st.header("Contact Me")
 
 with st.form(key="email_form"):
-    user_email = st.text_input("Your Email address")
-    raw_message = st.text_area("Your message")
+    user_email = st.text_input("Your Email address", placeholder='Currently Unavailable', disabled=True)
+    raw_message = st.text_area("Your message", placeholder='Currently Unavailable', disabled=True)
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
 {raw_message}"""
-    button = st.form_submit_button("Submit")
+    button = st.form_submit_button("Submit", disabled=True)
     if button:
         send_email(message)
         st.info("Your email was sent successfully")
+
+st.text("Check GitHub Repository(Top Right) ReadMe for clarification. Sorry for inconvenience")
